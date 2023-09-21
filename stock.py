78b885a3bb6d865c10446f4f54442d66f3320149
ash408 <124532093+ash408@users.noreply.github.com>
2023-09-21 07:21:04 -0500

    Setup basic constructor and profit calculator for Stock class

---
+++ stock.py
@@ -0,0 +1,12 @@
+
+
+class Stock:
+
+	def __init__(self, purchace_mark):
+		self.purchace_mark = purchace_mark
+
+	def calculate_profit_mark(percentage):
+		percent_mark = self.purchace_mark / 100
+		required_percentage = percent_mark * percentage
+
+		return self.purchace_mark + required_percentage
